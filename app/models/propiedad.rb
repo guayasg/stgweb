9b1d472ce5ca41fa2bdeca7a84a77f3fad8310e8
unknown <Informatica@.(none)>
2015-08-20 20:02:35 +0100

    Subo la estructura del proyecto al repositorio de GitHub

--- app/models/propiedad.rb
+++ app/models/propiedad.rb
@@ -1,3 +1,6 @@
 class Propiedad < ActiveRecord::Base
   self.table_name = "propiedades"
+  has_one :propiedadesComponer, foreign_key: "componertcorto_id"
+  has_one :propiedadesComponer, foreign_key: "componertlargo_id"
+  has_one :propiedadesComponer, foreign_key: "componertcomercial_id"
 end
